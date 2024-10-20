4fcdcabfb17d9c6a652720cebbc2dc406a2d0684
ronysharif <89507734+sharifkea@users.noreply.github.com>
2024-10-20 23:26:30 +0200

    API for All Categories
    All Categories Test done

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,10 +5,6 @@ using DataLayer;
 var ds = new DataService();
 
 
-//foreach(var item in )
-//{
-//    Console.WriteLine(item.Name);
-//}
 
 var od = ds.GetOrderDetailsByProductId(11);
 
@@ -18,14 +14,3 @@ Console.WriteLine(od.First().Order?.Date.ToString("yyyy-MM-dd"));
 Console.WriteLine(od.First().UnitPrice);
 Console.WriteLine(od.First().Quantity);
 
-/*
-foreach (var item in od)
-{
-    Console.WriteLine(item.Name);
-}
-10528	1997-05-06	21	3
-Assert.Equal(38, orderDetails.Count);
-Assert.Equal(10248, orderDetails.First().OrderId);
-Assert.Equal("1996-07-04", orderDetails.First().Order?.Date.ToString("yyyy-MM-dd"));
-Assert.Equal(14, orderDetails.First().UnitPrice);
-Assert.Equal(12, orderDetails.First().Quantity);*/
